e134ab4e632183de83d79ac638980eb3b3d94f7c
freyesdulib <fernando.reyes@du.edu>
2022-12-07 11:37:01 -0700

    initial import

---
+++ backend/controllers/repository_sync.rb
@@ -0,0 +1,52 @@
+class ArchivesSpaceService < Sinatra::Base
+
+  Endpoint.get('/repositories/:repo_id/archival_objects/:id/repository')
+    .description("Get a Repository JSON representation of an Archival Object")
+    .params(["id", :id],
+            ["repo_id", :repo_id])
+    .permissions([:view_repository])
+    .returns([200, "OK"]) \
+  do
+    obj = resolve_references(ArchivalObject.to_jsonmodel(params[:id]), ['linked_agents', 'subjects', 'digital_object', 'digital_object::tree'])
+    json = ASpaceExport.model(:repository).from_archival_object(JSONModel(:archival_object).new(obj))
+
+    json_response(ASpaceExport::serialize(json))
+  end
+
+  Endpoint.get('/repositories/:repo_id/archival_objects/:id/repository.xml')
+    .description("Get a MODS XML representation of an Archival Object")
+    .params(["id", :id],
+            ["repo_id", :repo_id])
+    .permissions([:view_repository])
+    .returns([200, "OK"]) \
+  do
+    obj = resolve_references(ArchivalObject.to_jsonmodel(params[:id]), ['linked_agents', 'subjects', 'digital_object', 'digital_object::tree'])
+    mods = ASpaceExport.model(:repository_mods).from_archival_object(JSONModel(:archival_object).new(obj))
+
+    xml_response(ASpaceExport::serialize(mods))
+  end
+
+  Endpoint.get('/repositories/:repo_id/archival_objects/:id/repository.:fmt/metadata')
+    .description("Get metadata for a MODS representation of an Archival Object")
+    .params(["id", :id],
+            ["repo_id", :repo_id])
+    .permissions([:view_repository])
+    .returns([200, "The export metadata"]) \
+  do
+    json_response({"filename" => "#{ArchivalObject[params[:id]].component_id}_mods.xml".gsub(/\s+/, '_'), "mimetype" => "application/xml"})
+  end
+
+  Endpoint.get('/repositories/:repo_id/resources/:id/repository')
+    .description("Get a Repository JSON representation of a Resource")
+    .params(["id", :id],
+            ["repo_id", :repo_id])
+    .permissions([:view_repository])
+    .returns([200, "OK"]) \
+  do
+    obj = resolve_references(Resource.to_jsonmodel(params[:id]), ['linked_agents', 'subjects', 'digital_object'])
+    json = ASpaceExport.model(:repository).from_resource(JSONModel(:resource).new(obj))
+
+    json_response(ASpaceExport::serialize(json))
+  end
+
+end
---
+++ backend/model/repository_exporter.rb
@@ -0,0 +1,118 @@
+class RepositorySerializer < ASpaceExport::Serializer
+  serializer_for :repository
+
+  include JSONModel
+
+  def serialize(mods, opts = {})
+    record = {
+      'title' => mods.title,
+      'uri' => mods.uri,
+      'identifiers' => [{'type': "local", 'identifier': mods.local_identifier}]
+    }
+
+    record['resource_type'] = mods.type_of_resource.gsub('_', ' ') unless mods.type_of_resource.nil?
+
+    unless mods.language_term.nil?
+      language = mods.language_term.split(":")
+      record['language'] = [{
+        'text' => language[0],
+        'code' => language[1],
+        'authority' => "iso639-2b"
+      }]
+    end
+
+    unless mods.dates.empty?
+      record['dates'] = []
+      mods.dates.each {|date| record['dates'].push(handle_date(date))}
+    end
+
+    unless mods.extents.empty?
+      record['extents'] = []
+      mods.extents.each {|extent| record['extents'].push(extent)}
+    end
+
+    record['subjects'] = []
+    mods.subjects.each do |subject|
+      subj = {
+        'authority': subject['source'],
+        'title': subject['title']
+      }
+
+      terms = []
+      unless subject['terms'].nil? or subject['terms'].empty?
+        subject['terms'].each {|term| terms.push({'type': term['type'], 'term': term['term']})}
+        subj['terms'] = terms
+      end
+
+      subj['authority_id'] = subject['authority_id'] if subject['authority_id']
+
+      record['subjects'].push(subj)
+    end
+
+    record['notes'] = []
+    unless mods.notes.empty?
+      mods.notes.each do |note|
+        record['notes'].push({
+          'type' => note['type'],
+          'content' => note['content']
+        })
+      end
+    end
+
+    record['names'] = []
+    unless mods.names.empty?
+      mods.names.each do |name|
+        record['names'].push name
+      end
+    end
+
+    record['parts'] = []
+    mods.parts.each {|part| record['parts'].push part} unless mods.parts.empty?
+    mods.parts.length > 1 ? record['is_compound'] = true : record['is_compound'] = false
+
+    record
+  end
+
+  private
+
+  def handle_date(date)
+    date_object = {
+      'encoding' => "w3cdtf",
+      'label' => date['label'],
+      'type' => date['date_type']
+    }
+
+    if date.has_key?('certainty')
+      date_object['qualifier'] = date['certainty']
+    end
+
+    # has an human-readable expression been provided?
+    has_expression = date.has_key?('expression') &&
+                  !date['expression'].nil? &&
+                  !date['expression'].empty?
+
+    # was a begin date provided?
+    has_begin = date.has_key?('begin') &&
+                !date['begin'].nil? &&
+                !date['begin'].empty?
+
+    # is the date a range? was an end date provided?
+    has_end = date.has_key?('end') &&
+              !date['end'].nil? &&
+              !date['end'].empty?
+
+    # assign the human-readable expression (to display to users)
+    if has_expression
+      date_object['expression'] = date['expression']
+    else
+      date_object['expression'] = "#{date['begin']}"
+      date_object['expression'] += "-#{date['end']}" if has_end
+    end
+
+    # assign the begin and end dates (to be used by elastic for indexing)
+    date_object['begin'] = date['begin'] if has_begin
+    date_object['end'] = date['end'] if has_end
+
+    date_object
+  end
+end
